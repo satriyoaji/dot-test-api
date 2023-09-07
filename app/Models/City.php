b6761a1b06a816c99ecad336dc10ec65a6c7f4e4
satriyoaji <satriyoaji27@gmail.com>
2023-09-08 05:51:40 +0700

    add model and migration

---
+++ app/Models/City.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+        'created_by',
+        'updated_by'
+    ];
+}
