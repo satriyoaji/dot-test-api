cedc8da9ec030e3bb5ffd6db057f943154e23e59
satriyoaji <satriyoaji27@gmail.com>
2023-09-08 23:00:31 +0700

    feature 1: swappable implementation in env var

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -10,13 +10,56 @@ class CityController extends Controller
 {
     public function search(Request $request)
     {
+        $dataSource = env('DATA_SOURCE');
+        if ($dataSource == "" || $dataSource == null) {
+            return new JsonResponse([
+                'message' => "DATA_SOURCE is must be set first",
+            ], 400);
+        }
+        if (strtolower($dataSource) !== "remote" && strtolower($dataSource) !== "database") {
+            return new JsonResponse([
+                'message' => "DATA_SOURCE is must be set either `REMOTE` or `DATABASE` value",
+            ], 400);
+        }
+
         $cityId = $request->input('id');
-        $city = City::findOrFail($cityId);
+
+        if (strtolower($dataSource) === "remote") {
+            return $this->searchRemote($cityId);
+        }
+        return $this->searchDatabase($cityId);
+    }
+
+    private function searchDatabase($requestId) {
+        $city = City::findOrFail($requestId);
         if (!$city) {
             return new JsonResponse([
                 'message' => "Data City by id not found",
             ], 404);
         }
-        return response()->json($city);
+        return response()->json([
+            'data' => $city
+        ]);
+    }
+
+    private function searchRemote($requestId) {
+        $client = new \GuzzleHttp\Client();
+        $apiKey = env('RAJAONGKIR_API_KEY');
+        if ($apiKey == null || $apiKey == "") {
+            return new JsonResponse([
+                'message' => "Error env var RAJAONGKIR_API_KEY not already set",
+            ], 400);
+        }
+        $responseCity = $client->get('https://api.rajaongkir.com/starter/city?id='.$requestId, [
+            'headers' => [
+                'key' => $apiKey
+            ]
+        ]);
+
+        $cities = json_decode($responseCity->getBody(), true);
+
+        return new JsonResponse([
+            'data' => $cities['rajaongkir']['results'],
+        ], $cities['rajaongkir']['status']['code']);
     }
 }
--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -10,13 +10,56 @@ class ProvinceController extends Controller
 {
     public function search(Request $request)
     {
+        $dataSource = env('DATA_SOURCE');
+        if ($dataSource == "" || $dataSource == null) {
+            return new JsonResponse([
+                'message' => "DATA_SOURCE is must be set first",
+            ], 400);
+        }
+        if (strtolower($dataSource) !== "remote" && strtolower($dataSource) !== "database") {
+            return new JsonResponse([
+                'message' => "DATA_SOURCE is must be set either `REMOTE` or `DATABASE` value",
+            ], 400);
+        }
+
         $provinceId = $request->input('id');
-        $province = Province::findOrFail($provinceId);
+
+        if (strtolower($dataSource) === "remote") {
+            return $this->searchRemote($provinceId);
+        }
+        return $this->searchDatabase($provinceId);
+    }
+
+    private function searchDatabase($requestId) {
+        $province = Province::findOrFail($requestId);
         if (!$province) {
             return new JsonResponse([
                 'message' => "Data Province by id not found",
             ], 404);
         }
-        return response()->json($province);
+        return response()->json([
+            'data' => $province
+        ]);
+    }
+
+    private function searchRemote($requestId) {
+        $client = new \GuzzleHttp\Client();
+        $apiKey = env('RAJAONGKIR_API_KEY');
+        if ($apiKey == null || $apiKey == "") {
+            return new JsonResponse([
+                'message' => "Error env var RAJAONGKIR_API_KEY not already set",
+            ], 400);
+        }
+        $responseProvince = $client->get('https://api.rajaongkir.com/starter/province?id='.$requestId, [
+            'headers' => [
+                'key' => $apiKey
+            ]
+        ]);
+
+        $provinces = json_decode($responseProvince->getBody(), true);
+
+        return new JsonResponse([
+            'data' => $provinces['rajaongkir']['results'],
+        ], $provinces['rajaongkir']['status']['code']);
     }
 }
